44d73fbcd26356521fa38d69d27b586415f07c25
Pedro Henrique Ferreira de Souza <pedro.souza@mondoin.com>
2024-04-10 12:35:22 -0400

    create components, add state, improve display

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import hearderIcon from '../assets/header-icon.svg'
+import { NavLink } from './nav-link'
 
 export function Header() {
   return (
@@ -6,8 +7,10 @@ export function Header() {
       <img src={hearderIcon} alt='header-icon' />
 
       <nav className='flex items-center gap-5'>
-        <a href='' className='font-medium text-sm text-zinc-300'>Events</a>
-        <a href='' className='font-medium text-sm'>Attendees</a>
+        {/* <a href='' className='font-medium text-sm text-zinc-300'>Events</a>
+        <a href='' className='font-medium text-sm'>Attendees</a> */}
+        <NavLink href='/events'>Events</NavLink>
+        <NavLink href='/attendees'>Attendees</NavLink>
       </nav>
     </div>
   )
